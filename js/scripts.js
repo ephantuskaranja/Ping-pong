42564a41336065506742080f3a432e3b6293d8c8
Ephantus karanja <ephantusslogan@gmail.com>
2018-05-03 13:56:45 +0300

    implement function ping with divisibility conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-function ping(i){
-    if (i % 3 == 0)
-{
-  var res = i.toString();
-  var ress=document.writeln(res.replace(res,'Ping'));
-}
-else
-{
-  document.writeln(i);
-}
+function ping(i) {
+    if (i % 15 == 0) {
+        var res = i.toString();
+        var ress = document.writeln(res.replace(res, 'PingPong'));
+    }
+    else if (i % 5 ==0){
+        var res3 = i.toString();
+        var ress3 = document.writeln(res3.replace(res3, 'Pong')); 
+    }
+    else if ((i % 3 == 0)){
+        var res2 = i.toString();
+        var ress2 = document.writeln(res2.replace(res2, 'Ping'));
+    }
+    else {
+        document.writeln(i);
+    }
 };
 
 $(document).ready(function () {
@@ -18,8 +24,8 @@ $(document).ready(function () {
 
         //display results
         $(".output").text(num);
-        for(var i = 1; i <= num; i++){
-           ping(i);
-        } 
+        for (var i = 1; i <= num; i++) {
+            ping(i);
+        }
     });
 });
